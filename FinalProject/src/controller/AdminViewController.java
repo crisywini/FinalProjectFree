a43b9eb0bd1017b53328ef51fa5c1ced2392fae9
Oscar Moreno <ormoreno2000@gmail.com>
2019-10-25 17:07:00 -0500

    CREACION DE LA VISTA DEL ADMIN
    Se creo la vista para el administrador con su respectivo controlador.

---
+++ FinalProject/src/controller/AdminViewController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import java.util.Observable;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import model.Espectaculo;
+
+public class AdminViewController {
+
+	private ObservableList<Espectaculo> espectaculos;
+	
+    @FXML
+    private Label adminLabel;
+
+    @FXML
+    private Button btnEstadisticas;
+
+    @FXML
+    private TableView<?> tablaEventos;
+
+    @FXML
+    private Button btnAgregar;
+
+    @FXML
+    private Button btnEliminar;
+
+    @FXML
+    private Button btnVer;
+    
+    public void agregarEspectaculo()
+    {
+    	Espectaculo nuevo = new Espectaculo();
+    	espectaculos
+    }
+
+}

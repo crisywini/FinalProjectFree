b0a3b9d2250c26522a16cb223df9dee2f712144e
Anjully <tatianamora1902@gmail.com>
2019-11-01 21:02:20 -0500

    ENVIO DE CORREOS
    en la clase correo se creo  un email desde el cual vamos a mandar los mensajes a otras personas. Usuario:analisisdealgoritmos12@gmail.com
    Contraseña:sergiotoc2

--- FinalProject/src/model/Boleta.java
+++ FinalProject/src/model/Boleta.java
@@ -4,31 +4,29 @@ public class Boleta {
 	private String id;
 	private Puesto miPuesto;
 	private Cliente miClienteAsociado;
-<<<<<<< Updated upstream
 	private double valor;
 	private Reserva miReservaAsociada;
+
 	/**
 	 * Metodo constructor vacio de la clase Boleta
 	 */
 	public Boleta() {
 		this("###", new Puesto(), new Cliente());
 	}
-	
+
 	/**
 	 * Metodo constructor de la clase Boleta
-	 * @param id de la boleta
-	 * @param miPuesto de la boleta
-	 * @param miClienteAsociado cliente asociado a la boleta 
+	 * 
+	 * @param id                de la boleta
+	 * @param miPuesto          de la boleta
+	 * @param miClienteAsociado cliente asociado a la boleta
 	 */
 	public Boleta(String id, Puesto miPuesto, Cliente miClienteAsociado) {
 		this.id = id;
 		this.miPuesto = miPuesto;
 		this.miClienteAsociado = miClienteAsociado;
 	}
-	
-=======
 
->>>>>>> Stashed changes
 	public String getId() {
 		return id;
 	}
@@ -54,23 +52,23 @@ public class Boleta {
 	}
 
 	public double getValor() {
-		if(getMiPuesto().getMiSeccionAsociada().getMiTipo()==TipoSeccion.PLATEA_1)
-			 setValor(380000);
-		if(getMiPuesto().getMiSeccionAsociada().getMiTipo()==TipoSeccion.PLATEA_2)
+		if (getMiPuesto().getMiSeccionAsociada().getMiTipo() == TipoSeccion.PLATEA_1)
+			setValor(380000);
+		if (getMiPuesto().getMiSeccionAsociada().getMiTipo() == TipoSeccion.PLATEA_2)
 			setValor(340000);
-		if(getMiPuesto().getMiSeccionAsociada().getMiTipo()==TipoSeccion.PLATEA_3)
+		if (getMiPuesto().getMiSeccionAsociada().getMiTipo() == TipoSeccion.PLATEA_3)
 			setValor(350000);
-		if(getMiPuesto().getMiSeccionAsociada().getMiTipo()==TipoSeccion.PRIMER_PISO_1)
+		if (getMiPuesto().getMiSeccionAsociada().getMiTipo() == TipoSeccion.PRIMER_PISO_1)
 			setValor(280000);
-		if(getMiPuesto().getMiSeccionAsociada().getMiTipo()==TipoSeccion.PRIMER_PISO_2)
+		if (getMiPuesto().getMiSeccionAsociada().getMiTipo() == TipoSeccion.PRIMER_PISO_2)
 			setValor(230000);
-		if(getMiPuesto().getMiSeccionAsociada().getMiTipo()==TipoSeccion.PRIMER_PISO_3)
+		if (getMiPuesto().getMiSeccionAsociada().getMiTipo() == TipoSeccion.PRIMER_PISO_3)
 			setValor(190000);
-		if(getMiPuesto().getMiSeccionAsociada().getMiTipo()==TipoSeccion.SEGUNDO_PISO_1)
+		if (getMiPuesto().getMiSeccionAsociada().getMiTipo() == TipoSeccion.SEGUNDO_PISO_1)
 			setValor(150000);
-		if(getMiPuesto().getMiSeccionAsociada().getMiTipo()==TipoSeccion.SEGUNDO_PISO_2)
+		if (getMiPuesto().getMiSeccionAsociada().getMiTipo() == TipoSeccion.SEGUNDO_PISO_2)
 			setValor(130000);
-		if(getMiPuesto().getMiSeccionAsociada().getMiTipo()==TipoSeccion.SEGUNDO_PISO_3)
+		if (getMiPuesto().getMiSeccionAsociada().getMiTipo() == TipoSeccion.SEGUNDO_PISO_3)
 			setValor(102000);
 		return valor;
 	}
@@ -79,17 +77,17 @@ public class Boleta {
 		this.valor = valor;
 	}
 
-
 	public Reserva getMiReservaAsociada() {
 		return miReservaAsociada;
 	}
-	
+
 	public void setMiReservaAsociada(Reserva miReservaAsociada) {
 		this.miReservaAsociada = miReservaAsociada;
 	}
+
 	@Override
 	public String toString() {
-		String info = "[id: "+id+" valor: "+getValor()+"]";
+		String info = "[id: " + id + " valor: " + getValor() + "]";
 		return info;
 	}
 }
